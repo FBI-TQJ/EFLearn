30e89c2589889c33aa3a7c16a16cbf1dabf31e02
FBI-TQJ <458173902@qq.com>
2018-05-21 17:28:37 +0800

    add data

--- CodeFirst/CodeFirst/Context.cs
+++ CodeFirst/CodeFirst/Context.cs
@@ -10,6 +10,11 @@ namespace CodeFirst
 {
     public class Context : DbContext
     {
+        //在这里，DbContext是所有基于EF的上下文基类，通过它可以访问到数据库中的所有表。
+        //上面的代码中调用了父类的构造函数，并且传入了一个键值对，键是name,值是FirstCodeFirstApp，
+        //这个键值对是定义在应用程序的配置文件中的，取决于你的应用程序类型，可能是app.config或者web.config。
+        //在我们的控制台应用程序中就是app.config。
+        //在app.config文件的configuration的节点下（不要在第一个节点下，否则报错）添加：
         public Context()
             : base("name=FirstCodeFirstApp")
         {
--- CodeFirst/CodeFirst/Program.cs
+++ CodeFirst/CodeFirst/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeFirst.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace CodeFirst
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.Database.CreateIfNotExists();//如果数据库不存在时则创建
+                var donators = new List<Donator>
+                {
+                    new Donator
+                    {
+                      Name   = "陈志康",
+                      Amount = 50,
+                      DonateDate = new DateTime(2016, 4, 7)
+                    },
+                    new Donator
+                    {
+                        Name = "海风",
+                        Amount = 5,
+                        DonateDate = new DateTime(2016, 4, 8)
+                    },
+                    new Donator
+                    {
+                        Name = "醉千秋",
+                        Amount = 18.8m,
+                        DonateDate = new DateTime(2016, 4, 15)
+                    }
+                };
+
+                context.Donators.AddRange(donators);
+                context.SaveChanges();
+            }
+            // Console.Write("DB has Created!");//提示DB创建成功
+            Console.Write("Creation Finished!");//提示创建完成
+            Console.Read();
         }
     }
 }
